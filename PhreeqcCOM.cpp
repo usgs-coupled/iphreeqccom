4924f8e83590e1cc3dfdc0488aba32ef8cad73da
Scott R Charlton <charlton@usgs.gov>
2006-12-01 00:07:06 +0000

    Added PhreeqcCOM project. Initial check in.
    git-svn-id: svn://136.177.114.72/svn_GW/IPhreeqcMMS/trunk/IPhreeqc/PhreeqcCOM@1523 1feff8c3-07ed-0310-ac33-dd36852eb9cd

---
+++ PhreeqcCOM.cpp
@@ -0,0 +1,66 @@
+// PhreeqcCOM.cpp : Implementation of DLL Exports.
+
+
+#include "stdafx.h"
+#include "resource.h"
+#include "PhreeqcCOM.h"
+
+
+class CPhreeqcCOMModule : public CAtlDllModuleT< CPhreeqcCOMModule >
+{
+public :
+	DECLARE_LIBID(LIBID_PhreeqcCOMLib)
+	DECLARE_REGISTRY_APPID_RESOURCEID(IDR_PHREEQCCOM, "{5EA2A828-7EAB-4E8A-9FEA-96D9DE684A9E}")
+};
+
+CPhreeqcCOMModule _AtlModule;
+
+
+#ifdef _MANAGED
+#pragma managed(push, off)
+#endif
+
+// DLL Entry Point
+extern "C" BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
+{
+	hInstance;
+    return _AtlModule.DllMain(dwReason, lpReserved); 
+}
+
+#ifdef _MANAGED
+#pragma managed(pop)
+#endif
+
+
+
+
+// Used to determine whether the DLL can be unloaded by OLE
+STDAPI DllCanUnloadNow(void)
+{
+    return _AtlModule.DllCanUnloadNow();
+}
+
+
+// Returns a class factory to create an object of the requested type
+STDAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, LPVOID* ppv)
+{
+    return _AtlModule.DllGetClassObject(rclsid, riid, ppv);
+}
+
+
+// DllRegisterServer - Adds entries to the system registry
+STDAPI DllRegisterServer(void)
+{
+    // registers object, typelib and all interfaces in typelib
+    HRESULT hr = _AtlModule.DllRegisterServer();
+	return hr;
+}
+
+
+// DllUnregisterServer - Removes entries from the system registry
+STDAPI DllUnregisterServer(void)
+{
+	HRESULT hr = _AtlModule.DllUnregisterServer();
+	return hr;
+}
+
