5bdba3bc5935e385646cad299b85f652f3db43af
Scott R Charlton <charlton@usgs.gov>
2006-12-02 04:46:26 +0000

    Finished adding methods/properties
    git-svn-id: svn://136.177.114.72/svn_GW/IPhreeqcMMS/trunk/IPhreeqc/PhreeqcCOM@1528 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- Resource.h
+++ Resource.h
@@ -2,9 +2,9 @@
 // Microsoft Visual C++ generated include file.
 // Used by PhreeqcCOM.rc
 //
-
 #define IDS_PROJNAME                    100
-#define IDR_PHREEQCCOM	101
+#define IDR_PHREEQCCOM                  101
+#define IDR_PHREEQC                     102
 
 // Next default values for new objects
 // 
@@ -13,6 +13,6 @@
 #define _APS_NEXT_RESOURCE_VALUE        201
 #define _APS_NEXT_COMMAND_VALUE         32768
 #define _APS_NEXT_CONTROL_VALUE         201
-#define _APS_NEXT_SYMED_VALUE           102
+#define _APS_NEXT_SYMED_VALUE           103
 #endif
 #endif
