1c20926dc32b033c24923fb9e778aea9fa8043d7
Scott R Charlton <charlton@usgs.gov>
2012-02-03 03:57:43 +0000

    changes made to increment version to 2.0 and allow per-user registration need t otest with VS2005
    git-svn-id: svn://136.177.114.72/svn_GW/IPhreeqcCOM/trunk@6163 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- IPhreeqcCOM/IPhreeqcCOM.cpp
+++ IPhreeqcCOM/IPhreeqcCOM.cpp
@@ -64,3 +64,35 @@ STDAPI DllUnregisterServer(void)
 	return hr;
 }
 
+
+#if 1
+// DllInstall - Adds/Removes entries to the system registry per user per machine.
+STDAPI DllInstall(BOOL bInstall, LPCWSTR pszCmdLine)
+{
+	HRESULT hr = E_FAIL;
+	static const wchar_t szUserSwitch[] = L"user";
+
+	if (pszCmdLine != NULL)
+	{
+		if (_wcsnicmp(pszCmdLine, szUserSwitch, _countof(szUserSwitch)) == 0)
+		{
+			ATL::AtlSetPerUserRegistration(true);
+		}
+	}
+
+	if (bInstall)
+	{	
+		hr = DllRegisterServer();
+		if (FAILED(hr))
+		{
+			DllUnregisterServer();
+		}
+	}
+	else
+	{
+		hr = DllUnregisterServer();
+	}
+
+	return hr;
+}
+#endif
